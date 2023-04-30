476645c315138fbd24151a835b1cf1fa416474ac
Zakee T <ZF.tanks@gmail.com>
2023-04-30 12:00:23 -0700

    Initial Commit

---
+++ WeatherMe/WeatherMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMeApp.swift
+//  WeatherMe
+//
+//  Created by Zakee Tanksley on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
