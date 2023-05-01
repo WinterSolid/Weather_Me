1a4bf15397a08b125bfef66dda2be3451c64c668
Zakee T <ZF.tanks@gmail.com>
2023-04-30 19:07:39 -0700

    Add urlRequest to Manager with Core Location"

--- WeatherMe/Managers/WeatherAPIManager.swift
+++ WeatherMe/Managers/WeatherAPIManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import CoreLocation
+
+class WeatherAPIManager {
+    func getCurrentWeather(latittude: CLLocationDegrees,longitude: CLLocationDegrees) {
+        guard let url = URL(string:
+                                "http://api.openweathermap.org/geo/1.0/reverse?lat=\(latittude)&lon=\(longitude)&limit={limit}&appid=\(KEY)&units=metric") else { fatalError("Missing URL")}
+        
+        let urlRequest = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {fatalError("Error Fetching Data") }
+        
+    }
+}
+// TODO: ADD REPONSE BODY JSON TO SWIFT
